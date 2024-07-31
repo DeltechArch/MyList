365de2d216cf5d2832c6ecc4b04423ee56fc4900
deltecharch <deltacharch@gmail.com>
2024-07-30 19:36:30 -0600

    Iniciando en windos 11

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,5 @@
 export function formatCurency(quantity){
-
+//Codigo para formatear los numeros  a dinero.
  return new Intl.NumberFormat('en-US',{
      style:'currency', currency: 'USD'
  }).format(quantity)
